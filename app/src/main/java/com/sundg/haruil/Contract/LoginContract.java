2e0d6752ebd0bc4b1990770b69dd1f8bdf28cf14
SoonDG <sans12@naver.com>
2024-09-01 22:19:00 +0900

    initial setting

---
+++ app/src/main/java/com/sundg/haruil/Contract/LoginContract.java
@@ -0,0 +1,12 @@
+package com.sundg.haruil.Contract;
+
+public interface LoginContract
+{
+    interface Presenter{
+
+    }
+
+    interface View{
+
+    }
+}
---
+++ app/src/main/java/com/sundg/haruil/Presenter/LoginPresenter.java
@@ -0,0 +1,7 @@
+package com.sundg.haruil.Presenter;
+
+import com.sundg.haruil.Contract.LoginContract;
+
+public class LoginPresenter implements LoginContract.Presenter {
+
+}
